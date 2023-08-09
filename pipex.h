275e808ef16710664caafeb3682dab4310146f78
ajakob <ajakob@student.42heilbronn.de>
2023-08-09 15:14:48 +0200

    Commit

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: ajakob <ajakob@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 01:17:49 by ajakob            #+#    #+#             */
-/*   Updated: 2023/07/30 17:06:52 by ajakob           ###   ########.fr       */
+/*   Updated: 2023/08/09 15:13:56 by ajakob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,7 @@
 # define PIPEX_H
 
 # include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
 # include <fcntl.h>
-# include <sys/types.h>
-# include <sys/wait.h>
 # include "libft/libft.h"
 
 typedef struct arg_s
@@ -29,7 +25,7 @@ typedef struct arg_s
 	char	*outfile;
 }	t_arg;
 
-char	*find_path(char *cmd, char *env[]);
+char	*find_path(char *cmd, char **env);
 char	**alloc_arr(char *cmd);
 void	ft_free(char **ptr);
 void	ft_error(char *str);
